0c2e19998442607a838778bf0c8deb5c2b83ef82
tianweidut <liutianweidlut@qq.com>
2013-03-10 13:45:36 +0800

    first commit for the website basic framework

---
+++ gui/admin.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 2012-11-5
+
+@author: tianwei
+'''
+
+from django.contrib import admin
+from gui.models import *
+
+RegisterClass =(
+                ActiveKeyInfo,
+                ActiveHistory,
+                ModelInfo,
+                CompoundInfo,
+                CompoundName,
+                CalculateHistory,
+                LanguageEnum,
+
+                )
+
+for item in RegisterClass:
+    admin.site.register(item)
+
+
