1b6ada45ad46c42b625b1e9782f5f97367286cff
tianweidut <liutianweidlut@qq.com>
2013-03-11 17:15:42 +0800

    delete piston

--- gui/urls.py
+++ gui/urls.py
@@ -5,7 +5,6 @@ Created on 2012-11-5
 @author: tianwei
 '''
 from django.conf.urls.defaults import *
-from piston.resource import Resource
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
